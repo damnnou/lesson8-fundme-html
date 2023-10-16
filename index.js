c46c2fb9e1d95bdb499a073e7f72001e02840706
damnnou <skdamn@outlook.com>
2023-10-16 22:50:14 +0300

    messenger connect

--- index.js
+++ index.js
@@ -1,75 +1,52 @@
-import { ethers } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.7.0/ethers.min.js";
-import { abi, contractAddress } from "./constants.js";
+const phoneDisplayHeader = document.getElementById('mtHeader');
+const inputValue = document.getElementById('messageInput');
+const messenger = document.getElementById('messenger');
 
-const connectButton = document.getElementById("connectButton");
-const fundButton = document.getElementById("fundButton");
-const withdrawButton = document.getElementById("withdrawButton");
-const getBalanceButton = document.getElementById("getBalanceButton");
-connectButton.onclick = connectWallet;
-fundButton.onclick = fund;
-withdrawButton.onclick = withdraw;
-getBalanceButton.onclick = getBalance;
 
-async function connectWallet() {
-  if (typeof window.ethereum !== "undefined") {
-    const wallet = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-    console.log("Connected with address - ", wallet);
-    connectButton.innerHTML = "Connected";
-  } else {
-    console.log("i dont see metamask :(");
-    connectButton.innerHTML = "Install Metamask first";
+let messageValue;
+
+if (!window.walletConnected) {
+  phoneDisplayHeader.innerHTML = 'Connect Wallet'
+}
+
+const delay = (message, time) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const messageDiv = document.createElement('div');
+      const messageBody = document.createElement('p');
+      messageDiv.classList.add('phone-display-body-messages-incoming');
+      messageDiv.appendChild(messageBody);
+      messageBody.innerHTML = message;
+      messenger.appendChild(messageDiv);
+      resolve();
+    }, time);
+  });
+};
+
+const welcomeMessages = async () => {
+  await delay('Hi!', 500);
+  await delay('For begin using Simple Wallet, you should connect your MetaMask!', 1000);
+  await delay('Just type "connect" in this chat to continue', 1000);
+};
+
+const sendMessage = () => {
+  const messageDiv = document.createElement('div');
+  const messageBody = document.createElement('p');
+
+  messageValue = inputValue.value;
+  messageDiv.classList.add('phone-display-body-messages-outgoing');
+  messageDiv.appendChild(messageBody);
+  messageBody.innerHTML = messageValue;
+  messenger.appendChild(messageDiv);
+
+  if (messageValue.toLowerCase().includes('connect')) {
+    window.connectWallet();
+  }
+
+  if (messageValue.toLowerCase().includes('connect')) {
+    window.connectWallet();
   }
 }
 
-async function getBalance() {
-  if (typeof window.ethereum !== "undefined") {
-    const provider = new ethers.BrowserProvider(window.ethereum);
-    const balance = await provider.getBalance(contractAddress);
-    console.log(ethers.formatEther(balance));
-  }
-}
-
-async function fund() {
-  const ethAmount = document.getElementById("ethAmount").value;
-  console.log(`Funding with ${ethAmount}...`);
-  if (typeof window.ethereum !== "undefined") {
-    const provider = new ethers.BrowserProvider(window.ethereum);
-    const signer = await provider.getSigner();
-    const contract = new ethers.Contract(contractAddress, abi, signer);
-    try {
-      const transactionResponse = await contract.fund({
-        value: ethers.parseEther(ethAmount),
-      });
-      await listenForTransactionMine(transactionResponse, provider);
-      console.log("Done!");
-    } catch (error) {
-      console.log(error);
-    }
-  }
-}
-
-async function withdraw() {
-  if (typeof window.ethereum !== "undefined") {
-    console.log(`Withdrawing...`);
-    const provider = new ethers.BrowserProvider(window.ethereum);
-    const signer = await provider.getSigner();
-    const contract = new ethers.Contract(contractAddress, abi, signer);
-    try {
-      const transactionResponse = await contract.withdraw();
-      await listenForTransactionMine(transactionResponse, provider);
-      console.log("Done!");
-    } catch (error) {
-      console.log(error);
-    }
-  }
-}
-
-async function listenForTransactionMine(transactionResponse, provider) {
-  console.log(`Mining ${transactionResponse.hash}...`);
-  const tx = await (
-    await provider.waitForTransaction(transactionResponse.hash)
-  ).confirmations();
-  console.log(`Completed with ${tx} confirmations`);
-}
+window.sendMessage = sendMessage;
+welcomeMessages();
---
+++ web3.js
@@ -0,0 +1,70 @@
+import { ethers } from "https://cdnjs.cloudflare.com/ajax/libs/ethers/6.7.0/ethers.min.js";
+import { abi, contractAddress } from "./constants.js";
+window.connectWallet = connectWallet;
+
+async function connectWallet() {
+  if (typeof window.ethereum !== "undefined") {
+    const wallet = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    const phoneDisplayHeader = document.getElementById('mtHeader');
+    console.log("Connected with address - ", wallet);
+    const shortenAddress = wallet.toString().slice(0, 6) + "..." + wallet.toString().slice(35, 42);
+    window.walletConnected = wallet;
+    phoneDisplayHeader.innerHTML = shortenAddress;
+  } else {
+    console.log("i dont see metamask :(");
+    connectButton.innerHTML = "Install Metamask first";
+  }
+}
+
+async function getBalance() {
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const balance = await provider.getBalance(contractAddress);
+    console.log(ethers.formatEther(balance));
+  }
+}
+
+async function fund() {
+  const ethAmount = document.getElementById("ethAmount").value;
+  console.log(`Funding with ${ethAmount}...`);
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      const transactionResponse = await contract.fund({
+        value: ethers.parseEther(ethAmount),
+      });
+      await listenForTransactionMine(transactionResponse, provider);
+      console.log("Done!");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+async function withdraw() {
+  if (typeof window.ethereum !== "undefined") {
+    console.log(`Withdrawing...`);
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      const transactionResponse = await contract.withdraw();
+      await listenForTransactionMine(transactionResponse, provider);
+      console.log("Done!");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+async function listenForTransactionMine(transactionResponse, provider) {
+  console.log(`Mining ${transactionResponse.hash}...`);
+  const tx = await (
+    await provider.waitForTransaction(transactionResponse.hash)
+  ).confirmations();
+  console.log(`Completed with ${tx} confirmations`);
+}
